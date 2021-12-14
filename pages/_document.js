33c4cfc7fc33ac753ea921a688e89fce8b8c91be
Steven Valles <s.valles94@hotmail.com>
2021-12-14 01:59:35 -0600

    Optimization

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,8 @@ class MyDocument extends Document {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" />
-        <link rel="preload" as="style" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />        </Head>
+        <link rel="stylesheet" href="/fontaw/css/all.min.css" />        
+        </Head>
         <body>
           <Main />
           <NextScript />
