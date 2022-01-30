b5000da75ba6dec6c0ee3f1a2eb88759c2ddafa2
Steven Valles <s.valles94@hotmail.com>
2022-01-30 14:48:04 -0600

    Changes

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -2,7 +2,7 @@ import Header from '../UI/Header/header';
 import SideNav from '../UI/SideNav/SideNav';
 
 const MainLayout = (props) => {
-    return(<div style={{ 
+    return(<div className="mainlayout" style={{ 
         background: 'linear-gradient(135deg, rgba(0,0,0,1) 50%, rgba(119,30,135,1) 100%)',
         minHeight: '100vh',
         backgroundAttachment: 'fixed'
